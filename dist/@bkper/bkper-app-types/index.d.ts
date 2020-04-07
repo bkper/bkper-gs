16f1cc4dbbbbcd71a703c819e657d17be0372264
mael <mael@bkper.com>
2020-04-07 11:00:04 -0300

    Updating docs

--- dist/@bkper/bkper-app-types/index.d.ts
+++ dist/@bkper/bkper-app-types/index.d.ts
@@ -908,10 +908,15 @@ declare namespace bkper {
      *
      * Implement your own if you need to use one other than the default built-in [ScriptApp](https://developers.google.com/apps-script/reference/script/script-app#getoauthtoken).
      * 
-     * See
+     * Its specially usefull on environments where you can use the built-in ScriptApp services such as [Custom Functions in Google Sheets](https://developers.google.com/apps-script/guides/sheets/functions).
+     * 
+     * Learn more how to [OAuth 2 library](https://github.com/gsuitedevs/apps-script-oauth2) for Google Apps Script
      */
     export interface OAuthTokenProvider {
 
+        /**
+         * A valid OAuth2 access token with **email** scope authorized.
+         */
         getOAuthToken(): string;
 
     }
--- src/API_.ts
+++ src/API_.ts
@@ -7,11 +7,17 @@ let OAUTH_TOKEN_PROVIDER_: OAuthTokenProvider;
  * 
  * Implement your own if you need to use one other than the default built-in [ScriptApp](https://developers.google.com/apps-script/reference/script/script-app#getoauthtoken).
  * 
- * See
+ * Its specially usefull on environments where you can use the built-in ScriptApp services such as [Custom Functions in Google Sheets](https://developers.google.com/apps-script/guides/sheets/functions).
+ * 
+ * Learn more how to [OAuth 2 library](https://github.com/gsuitedevs/apps-script-oauth2) for Google Apps Script
  * 
  * @public
  */
 interface OAuthTokenProvider {
+
+  /**
+   * A valid OAuth2 access token with **email** scope authorized.
+   */
   getOAuthToken(): string;
 }
 

e0ba1217cdfc737437f5d74ad7f79fad97a60703
mael <mael@bkper.com>
2020-05-07 19:35:06 -0300

    Using http-request-app library

--- src/HttpApiRequest.ts
+++ src/HttpApiRequest.ts
@@ -50,113 +50,44 @@ function setOAuthTokenProvider(tokenProvider: OAuthTokenProvider) {
 }
 
 
-class HttpRequest {
-  private url: string;
-  private params: Array<{name: string, value: string}>;
-  private options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {};
+class HttpApiRequest  {
 
-  constructor(url: string) {
-    let parts = url.split('?');
-    this.url = parts[0];
-    if (parts.length == 2) {
-      let params = parts[1].split('&');
-      params.forEach(param => {
-        let keyValue = param.split('=');
-        if (keyValue.length == 2) {
-          this.addParam(keyValue[0], keyValue[1])
-        }
-      });
-    }
+  private httpRequest: Bkper.HttpRequest;
+
+  constructor(path: string) {
+    this.httpRequest = HttpRequestApp.newRequest(`https://app.bkper.com/_ah/api/bkper/v2/${path}`);
   }
 
-  public setMethod(method: GoogleAppsScript.URL_Fetch.HttpMethod): HttpRequest {
-    this.options.method = method;
+  public setMethod(method: GoogleAppsScript.URL_Fetch.HttpMethod) {
+    this.httpRequest.setMethod(method);
     return this;
   }
   
 
-  public setHeader(name: string, value: string): HttpRequest {
-    if (this.options.headers == null) {
-      this.options.headers = {} as GoogleAppsScript.URL_Fetch.HttpHeaders;
-    }
-    this.options.headers[name] = value;
+  public setHeader(name: string, value: string) {
+    this.httpRequest.setHeader(name, value);
     return this;
   }
 
-  public addParam(name: string, value: any): HttpRequest {
-    if (this.params == null) {
-      this.params = [];
-    }
-    this.params.push({name, value});
+  public addParam(name: string, value: any) {
+    this.httpRequest.addParam(name, value)
     return this;
   }
 
-  public setContentType(contentType: string): HttpRequest {
-    this.options.contentType = contentType;
+  public setContentType(contentType: string) {
+    this.httpRequest.setContentType(contentType);
     return this;
   }
 
   public getContentType(): string {
-    return this.options.contentType;
+    return this.httpRequest.getContentType();
   }
 
-  public setPayload(payload: any): HttpRequest {
-    this.options.payload = payload;
+  public setPayload(payload: GoogleAppsScript.URL_Fetch.Payload) {
+    this.httpRequest.setPayload(payload);
     return this;
   }
 
-  public setValidateHttpsCertificates(validateHttpsCertificates: boolean): HttpRequest {
-    this.options.validateHttpsCertificates = validateHttpsCertificates;
-    return this;
-  }
-
-  public setFollowRedirects(followRedirects: boolean): HttpRequest {
-    this.options.followRedirects = followRedirects;
-    return this;
-  }
-
-  public setMuteHttpExceptions(muteHttpExceptions: boolean): HttpRequest {
-    this.options.muteHttpExceptions = muteHttpExceptions;
-    return this;
-  }
-
-  public getUrl(): string {
-    let url = this.url;
-    if (this.params != null) {
-      let i = 0
-      if (url.indexOf('?') < 0) {
-        url += '?';
-      } else {
-        i++;
-      }
-      for (const param of this.params) {
-          if (i > 0) {
-            url += "&";
-          }
-          var key = param.name;
-          var value = param.value;          
-          if (value != null) {
-            url += key + "=" + encodeURIComponent(value);
-            i++;
-          }
-      }      
-
-    }
-    return url
-  }
-
-  public fetch(): GoogleAppsScript.URL_Fetch.HTTPResponse {
-    return UrlFetchApp.fetch(this.getUrl(), this.options);
-  }; 
-
-}
-
-
-
-class HttpApiRequest extends HttpRequest {
-  constructor(path: string) {
-    super(`https://app.bkper.com/_ah/api/bkper/v2/${path}`)
-  }
 
   fetch(): GoogleAppsScript.URL_Fetch.HTTPResponse {
     if (OAUTH_TOKEN_PROVIDER_ == null) {
@@ -167,17 +98,17 @@ class HttpApiRequest extends HttpRequest {
       API_KEY_ = CachedProperties_.getCachedProperty(CacheService.getScriptCache(), PropertiesService.getScriptProperties(), 'API_KEY');
     }
 
-    this.setHeader('Authorization', `Bearer ${OAUTH_TOKEN_PROVIDER_.getOAuthToken()}`);
-    this.addParam('key', API_KEY_);
-    if (this.getContentType() == null) {
-      this.setContentType('application/json; charset=UTF-8')
+    this.httpRequest.setHeader('Authorization', `Bearer ${OAUTH_TOKEN_PROVIDER_.getOAuthToken()}`);
+    this.httpRequest.addParam('key', API_KEY_);
+    if (this.httpRequest.getContentType() == null) {
+      this.httpRequest.setContentType('application/json; charset=UTF-8')
     }
-    this.setMuteHttpExceptions(true);
+    this.httpRequest.setMuteHttpExceptions(true);
 
     var retries = 0;
     var sleepTime = 1000;
     while (true) {
-      var response = super.fetch();
+      var response = this.httpRequest.fetch();
       if (response.getResponseCode() >= 200 && response.getResponseCode() < 300) {
         //OK
         return response;      
@@ -200,7 +131,6 @@ class HttpApiRequest extends HttpRequest {
         }
       }
     }
-    
   }
 }
 

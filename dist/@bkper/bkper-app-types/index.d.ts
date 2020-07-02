7cf3fa9a3ea7163f5cbae7bb19a45f635b517363
mael <mael@bkper.com>
2020-07-02 16:55:42 -0300

    Publishing types

--- dist/@bkper/bkper-app-types/index.d.ts
+++ dist/@bkper/bkper-app-types/index.d.ts
@@ -276,6 +276,13 @@ declare namespace Bkper {
         formatValues(format: boolean): BalancesDataTableBuilder;
 
         /**
+         * Defines whether the dates should be hidden for **PERIOD** or **CUMULATIVE** [[BalanceType]]
+         *
+         * @returns This builder with respective hide dates option, for chaining.
+         */
+        hideDates(hide: boolean): BalancesDataTableBuilder;
+
+        /**
          * Defines wheter should rows and columns should be transposed.
          *
          * For **TOTAL** [[BalanceType]], the **transposed** table looks like:

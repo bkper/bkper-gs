b67906e09bfef3492caf1177a40320611f018165
mael <mael@bkper.com>
2020-08-09 23:15:03 -0300

    Ensure queries list initilized

--- src/SavedQueryService_.ts
+++ src/SavedQueryService_.ts
@@ -3,7 +3,7 @@ namespace SavedQueryService_ {
   export function getSavedQueries(bookId: string): bkper.Query[] {
     var responseJSON = new HttpBooksApiV3Request(`${bookId}/queries`).fetch().getContentText();
     var savedQueriesPlain = JSON.parse(responseJSON);
-    if (savedQueriesPlain == null) {
+    if (savedQueriesPlain == null || savedQueriesPlain.items == null) {
       return [];
     } else {
       return savedQueriesPlain.items;

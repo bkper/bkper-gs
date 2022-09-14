fafedd46c2f6971112f2b4f0c8889dccf3617052
victordrumond <victordrumond@hotmail.com>
2022-09-14 10:40:14 -0300

    Add includeGroups to AccountsDataTableBuilder

--- src/AccountsDataTableBuilder.ts
+++ src/AccountsDataTableBuilder.ts
@@ -1,5 +1,5 @@
 /**
- * A AccountsDataTableBuilder is used to setup and build two-dimensional arrays containing transactions.
+ * A AccountsDataTableBuilder is used to setup and build two-dimensional arrays containing accounts.
  * 
  * @public
  */
@@ -8,10 +8,12 @@ class AccountsDataTableBuilder {
   private accounts: Account[];
 
   private shouldIncludeArchived: boolean;
+  private shouldAddGroups: boolean;
 
   constructor(accounts: Account[]) {
     this.accounts = accounts;
     this.shouldIncludeArchived = false;
+    this.shouldAddGroups = false;
   }
 
   /**
@@ -21,6 +23,16 @@ class AccountsDataTableBuilder {
    */
   public includeArchived(include: boolean): AccountsDataTableBuilder {
     this.shouldIncludeArchived = include;
+    return this;
+  }
+
+  /**
+   * Defines whether include account groups.
+   * 
+   * @returns This builder with respective include groups option, for chaining.
+   */
+  public includeGroups(include: boolean): AccountsDataTableBuilder {
+    this.shouldAddGroups = include;
     return this;
   }
 
@@ -37,11 +49,23 @@ class AccountsDataTableBuilder {
     return 3;
   }
 
+  private getMaxNumberOfGroups(): number {
+    let maxNumberOfGroups = 0;
+    for (const account of this.accounts) {
+      const groups = account.getGroups();
+      if (groups.length > maxNumberOfGroups) {
+        maxNumberOfGroups = groups.length;
+      }
+    }
+    return maxNumberOfGroups;
+  }
+
   /**
    * @returns A two-dimensional array containing all [[Accounts]].
    */
   public build(): any[][] {
-    var table = new Array<Array<any>>();
+
+    let table = new Array<Array<any>>();
 
     let accounts = this.accounts;
 
@@ -50,35 +74,37 @@ class AccountsDataTableBuilder {
     }
 
     let headers = [];
-    headers.push('Name')
-    headers.push('Type')
+    headers.push('Name');
+    headers.push('Type');
+
+    if (this.shouldAddGroups) {
+      for (let i = 0; i < this.getMaxNumberOfGroups(); i++) {
+        headers.push('Group');
+      }
+    }
 
     accounts.sort((a1: Account, a2: Account) => {
       let ret = this.getTypeIndex(a1.getType()) - this.getTypeIndex(a2.getType())
       if (ret == 0) {
-        ret = a1.getNormalizedName().localeCompare(a2.getNormalizedName())
+        ret = a1.getNormalizedName().localeCompare(a2.getNormalizedName());
       }
       return ret;
     })
 
-
     for (const account of accounts) {
-      var line = new Array();
-      line.push(account.getName())
-      line.push(account.getType())
 
-      let groups = account.getGroups();
+      let line = new Array();
+      line.push(account.getName());
+      line.push(account.getType());
 
-      groups.sort((g1: Group, g2: Group) => {
-        return g1.getNormalizedName().localeCompare(g2.getNormalizedName())
-      })
-
-      for (const group of groups) {
-        line.push(group.getName())
-      }
-
-      if (line.length > headers.length) {
-        headers.push('Group')
+      if (this.shouldAddGroups) {
+        const groups = account.getGroups();
+        groups.sort((g1: Group, g2: Group) => {
+          return g1.getNormalizedName().localeCompare(g2.getNormalizedName());
+        })
+        for (const group of groups) {
+          line.push(group.getName());
+        }
       }
 
       table.push(line);
@@ -93,4 +119,3 @@ class AccountsDataTableBuilder {
   }
 
 }
-

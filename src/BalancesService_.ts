4c518a952e3cc190fc4ac76fcd664cb721bf4290
Bruno Coelho <bbcoelho@gmail.com>
2025-08-06 22:06:27 -0300

    feat: get balance from balancesUrl

--- src/BalancesService_.ts
+++ src/BalancesService_.ts
@@ -3,8 +3,13 @@ namespace BalancesService_ {
   export function getBalances(bookId: string, query: string): bkper.Balances {
     var responseJSON = new HttpBooksApiV5Request(`${bookId}/balances`).addParam('query', query).addParam('time', Date.now()).fetch().getContentText();
     var report = JSON.parse(responseJSON);
+
+    if (report.balancesUrl) {
+      responseJSON = UrlFetchApp.fetch(`${report.balancesUrl}`).getContentText();
+      report = JSON.parse(responseJSON);
+    }
+
     return report;
   }
   
 }
-

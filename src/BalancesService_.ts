ef20a8290896745f46f4c0a3c45ab2584b8bfccf
Bruno Coelho <bbcoelho@gmail.com>
2025-08-08 17:09:03 -0300

    feat: fetch object from Cloud Storage

--- src/BalancesService_.ts
+++ src/BalancesService_.ts
@@ -4,23 +4,15 @@ namespace BalancesService_ {
     var responseJSON = new HttpBooksApiV5Request(`${bookId}/balances`).addParam('query', query).addParam('time', Date.now()).fetch().getContentText();
     var report = JSON.parse(responseJSON);
 
-    // if (report.balancesUrl) {
-    //   responseJSON = UrlFetchApp.fetch(`${report.balancesUrl}`).getContentText();
-    //   report = JSON.parse(responseJSON);
-    // }
-
-    if (!report.balancesUrl) {
+    // for payloads larger than 32MB, fetch object from Cloud Storage
+    if (report.balancesUrl) {
       try {
-        const responseJSON = UrlFetchApp.fetch('https://storage.googleapis.com/bkper-public-temp-short/temp_8f630e48-7c78-4c04-a51d-9597afedf046.json');
-        console.log(JSON.parse(responseJSON.getContentText()));
+        responseJSON = UrlFetchApp.fetch(`${report.balancesUrl}`).getContentText();
+        report = JSON.parse(responseJSON);
       } catch (error) {
         console.log(error);
       }
     }
-
-    // if (report.balancesUrl) {
-    //   console.log(report.balancesUrl);
-    // }
 
     return report;
   }
